3803bce163bcb93b187620608001c1bb8b09a233
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-04-12 18:25:45 +0300

    feat: add solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  return domains.reduce((acc, val) => {
+    let match1, match2, match3;
+    if (val.match(/(\.\w+$)/)) match1 = val.match(/(\.\w+$)/)[0]
+    if (val.match(/(\.\w+){2}/)) match2 = val.match(/(\.\w+){2}/)[0].replace('.', '').split('.').reverse().map(el => ('.' + el)).join('');
+    match3 = val.split('.').reverse().map(el => ('.' + el)).join('');
+    let matches = [match1, match2, match3];
+    for (let match of matches) {
+      if (match) {
+        if (match in acc) acc[match]++;
+        else acc[match] = 1;
+      }
+    }
+    return acc;
+  }, {});
 }
 
 module.exports = {
